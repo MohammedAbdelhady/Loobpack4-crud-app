a4d009dc012cf29f0468e82f4eae9d9e249b81ee
Mohamed Gomaa <promgom3a@gmail.com>
2021-08-15 13:04:17 +0200

    add employee model

---
+++ src/models/employee.model.ts
@@ -0,0 +1,46 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Employee extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  lastName: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  age: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  position: string;
+
+
+  constructor(data?: Partial<Employee>) {
+    super(data);
+  }
+}
+
+export interface EmployeeRelations {
+  // describe navigational properties here
+}
+
+export type EmployeeWithRelations = Employee & EmployeeRelations;
